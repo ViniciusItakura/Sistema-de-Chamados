e7ce8342402a619e3867bbd318fd46ebaa339311
ViniciusItakura <viniciusitakura1@gmail.com>
2023-12-11 23:42:37 -0300

    Finalizado projeto

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -7,6 +7,8 @@ import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import { collection, getDocs, orderBy, limit, startAfter, query } from 'firebase/firestore';
 import { db } from '../../services/firebaseConnection';
+import { format } from 'date-fns';
+import Modal from '../../components/Modal';
 
 const listRef = collection(db, "chamados");
 
@@ -15,12 +17,17 @@ export default function Dashboard() {
     const [chamados, setChamados] = useState([]);
     const [loading, setLoading] = useState(true);
     const [isEmpty, setIsEmpty] = useState(false);
+    const [lastDocs, setLastDocs] = useState();
+    const [loadingMore, setLoadingMore] = useState(false);
+    const [showPostModal, setShowPostModal] = useState(false);
+    const [detail, setDetail] = useState();
 
     useEffect(() => {
         async function loadChamados() {
             const q = query(listRef, orderBy('created', 'desc'), limit(5));
 
             const querySnapshot = await getDocs(q);
+            setChamados([]);
             await updateState(querySnapshot);
 
             setLoading(false);
@@ -45,13 +52,48 @@ export default function Dashboard() {
                     created: doc.data().created,
                     status: doc.data().status,
                     complemento: doc.data().complemento,
+                    createdFormat: format(doc.data().created.toDate(), 'dd/MM/yyy'),
                 });
             });
 
+
+            const lastDoc = querySnapshot.docs[querySnapshot.docs.length - 1];
+            setLastDocs(lastDoc);
             setChamados(chamados => [...chamados, ...lista]);
         } else {
             setIsEmpty(true);
         }
+
+        setLoadingMore(false);
+    }
+
+    async function handleMore() {
+        setLoadingMore(true);
+        const q = query(listRef, orderBy('created', 'desc'), startAfter(lastDocs), limit(5));
+        const querySnapshot = await getDocs(q);
+        await updateState(querySnapshot);
+    }
+
+    if (loading) {
+        return (
+            <div>
+                <Header />
+                <div className='content'>
+                    <Title name='Tickets'>
+                        <FiMessageSquare size={25} />
+                    </Title>
+
+                    <div className='container dashboard'>
+                        <span>Buscando chamados...</span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    function toggleModal(item) {
+        setShowPostModal(!showPostModal);
+        setDetail(item);
     }
 
     return (
@@ -90,29 +132,40 @@ export default function Dashboard() {
                                 </thead>
 
                                 <tbody>
-                                    <tr>
-                                        <td data-label="Cliente">Mercado Esquina</td>
-                                        <td data-label="Assunto">Suporte</td>
-                                        <td data-label="Status">
-                                            <span className='badge' style={{ backgroundColor: '#999' }}>Em aberto</span>
-                                        </td>
-                                        <td data-label="Cadastrado">08/12/2023</td>
-                                        <td data-label="#">
-                                            <button className='action' style={{ backgroundColor: '#3583f6' }}>
-                                                <FiSearch color='#fff' size={17} />
-                                            </button>
-                                            <button className='action' style={{ backgroundColor: '#f6a935' }}>
-                                                <FiEdit2 color='#fff' size={17} />
-                                            </button>
-                                        </td>
-                                    </tr>
+                                    {chamados.map((item, index) => {
+                                        return (
+                                            <tr key={index}>
+                                                <td data-label="Cliente">{item.cliente}</td>
+                                                <td data-label="Assunto">{item.assunto}</td>
+                                                <td data-label="Status">
+                                                    <span className='badge' style={{ backgroundColor: item.status === 'Aberto' ? '#5cb85c' : '#999' }}>{item.status}</span>
+                                                </td>
+                                                <td data-label="Cadastrado">{item.createdFormat}</td>
+                                                <td data-label="#">
+                                                    <button onClick={() => toggleModal(item)} className='action' style={{ backgroundColor: '#3583f6' }}>
+                                                        <FiSearch color='#fff' size={17} />
+                                                    </button>
+                                                    <Link to={`/new/${item.id}`} className='action' style={{ backgroundColor: '#f6a935' }}>
+                                                        <FiEdit2 color='#fff' size={17} />
+                                                    </Link>
+                                                </td>
+                                            </tr>
+                                        );
+                                    })}
                                 </tbody>
                             </table>
+
+                            {loadingMore && <h3>Buscando mais chamados...</h3>}
+                            {!loadingMore && !isEmpty && <button onClick={handleMore} className='btn-more'>Buscar mais</button>}
                         </>
                     )}
 
                 </>
             </div>
+
+            {showPostModal && (
+                <Modal conteudo={detail} close={() => setShowPostModal(!showPostModal)} />
+            )}
         </div>
     );
 }
--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -5,8 +5,9 @@ import { FiPlusCircle } from 'react-icons/fi';
 
 import { AuthContex } from '../../contexs/auth';
 import { db } from '../../services/firebaseConnection';
-import { collection, getDocs, getDoc, doc, addDoc } from 'firebase/firestore';
+import { collection, getDocs, getDoc, doc, addDoc, updateDoc } from 'firebase/firestore';
 import { toast } from 'react-toastify';
+import { useParams, useNavigate } from 'react-router-dom';
 
 import './new.css';
 
@@ -14,14 +15,17 @@ const listRef = collection(db, "customers");
 
 export default function New() {
     const { user } = useContext(AuthContex);
+    const { id } = useParams();
+    const navigate = useNavigate();
 
-    const [customers, setCustomers] = useState([])
+    const [customers, setCustomers] = useState([]);
     const [loadCustomer, setLoadCustomer] = useState(true);
-    const [customerSelected, setCustomerSelected] = useState(0)
+    const [customerSelected, setCustomerSelected] = useState(0);
 
-    const [complemento, setComplemento] = useState('')
-    const [assunto, setAssunto] = useState('Suporte')
-    const [status, setStatus] = useState('Aberto')
+    const [complemento, setComplemento] = useState('');
+    const [assunto, setAssunto] = useState('Suporte');
+    const [status, setStatus] = useState('Aberto');
+    const [idCustomer, setIdCustomer] = useState(false);
 
     useEffect(() => {
         async function loadCustomers() {
@@ -46,6 +50,10 @@ export default function New() {
                     setCustomers(lista);
                     setLoadCustomer(false);
 
+                    if (id) {
+                        loadId(lista);
+                    }
+
                 })
                 .catch((error) => {
                     console.log("ERRRO AO BUSCAR OS CLIENTES", error)
@@ -55,7 +63,23 @@ export default function New() {
         }
 
         loadCustomers();
-    }, [])
+    }, [id]);
+
+    async function loadId(lista) {
+        const docRef = doc(db, 'chamados', id);
+        await getDoc(docRef).then((snapshot) => {
+            setAssunto(snapshot.data().assunto);
+            setStatus(snapshot.data().status);
+            setComplemento(snapshot.data().complemento);
+
+            let index = lista.findIndex(item => item.id === snapshot.data().clienteId);
+            setCustomerSelected(index);
+            setIdCustomer(true);
+        }).catch((erro) => {
+            console.log(erro);
+            setIdCustomer(false);
+        });
+    }
 
 
     function handleOptionChange(e) {
@@ -73,6 +97,26 @@ export default function New() {
 
     async function handleRegister(e) {
         e.preventDefault();
+
+        if (idCustomer) {
+            const docRef = doc(db, 'chamados', id);
+            await updateDoc(docRef, {
+                cliente: customers[customerSelected].nomeFantasia,
+                clienteId: customers[customerSelected].id,
+                assunto: assunto,
+                complemento: complemento,
+                status: status,
+                userId: user.uid,
+            }).then(() => {
+                toast.info("Chamado atualizado com sucesso!");
+                setCustomerSelected(0);
+                setComplemento('');
+                navigate('/dashboard');
+            }).catch((error) => {
+                toast.error("Ops, erro ao atualizar este chamado!");
+            });
+            return;
+        }
 
         //Registrar chamado
         await addDoc(collection(db, "chamados"), {
@@ -98,7 +142,7 @@ export default function New() {
             <Header />
 
             <div className="content">
-                <Title name="Novo chamado">
+                <Title name={id ? "Editando Chamado" : "Novo chamado"}>
                     <FiPlusCircle size={25} />
                 </Title>
 
